e6c77878e2de69ab64dade03708358750b762ae0
buithanhbavuong <bavuongco10@gmail.com>
2018-04-18 22:12:40 +0700

    editable

--- src/Components/Note/Note.component.js
+++ src/Components/Note/Note.component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FaTrash from 'react-icons/lib/fa/trash';
 
 import WithTransform from '../../Components/WithTransform/WithTransform.component'
 import './Note.component.css';
@@ -9,7 +10,12 @@ const Note = ({ note }) => {
       {
         ({top, left, transform}) =>
           <div className="note" style={{top, left, transform}}>
-            {note}
+            <form>
+              <div className="controls">
+                <button><FaTrash /></button>
+              </div>
+              <textarea placeholder={note} required/>
+            </form>
           </div>
       }
     </WithTransform>
